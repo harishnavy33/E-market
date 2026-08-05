cecd63d5805856cfd05a44b29cbb41b214f0cedf
harish <harishnavy33@gmail.com>
2026-08-05 21:41:24 +0530

    Added SanityTest.java

---
+++ SanityTest.java
@@ -0,0 +1,11 @@
+package com.harishmart;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SanityTest {
+    @Test
+    void projectBuildsAndTestsRun() {
+        assertEquals(4, 2 + 2);
+    }
+}
